20945afe9ec69daa32c9acb82c4eb02078270f47
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-19 21:59:57 +0200

    Add example functions - scalar times2

---
+++ src/test/java/com/teradata/functions/TestExampleFunctions.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.teradata.functions;
+
+import com.facebook.presto.operator.scalar.FunctionAssertions;
+import com.facebook.presto.spi.type.Type;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+
+public class TestExampleFunctions
+{
+    private FunctionAssertions functionAssertions;
+
+    @BeforeClass
+    public void setUp()
+    {
+        functionAssertions = new FunctionAssertions().addScalarFunctions(ExampleFunctions.class);
+    }
+
+    @Test
+    public void testTimes2()
+    {
+        assertFunction("times2(CAST(1.0 AS BIGINT))", BIGINT, 2L);
+        assertFunction("times2(CAST(0.0 AS BIGINT))", BIGINT, 0L);
+        assertFunction("times2(CAST(-1.0 AS BIGINT))", BIGINT, -2L);
+    }
+
+    private void assertFunction(String projection, Type expectedType, Object expected)
+    {
+        functionAssertions.assertFunction(projection, expectedType, expected);
+    }
+}
