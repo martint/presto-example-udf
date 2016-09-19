cd8316f6fb822f1b1e67205e40c69dbfa820bc10
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-19 22:23:06 +0200

    Add example aggregate function - countIf2

---
+++ src/main/java/com/teradata/functions/CountIfTwoAggregateFunction.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.teradata.functions;
+
+import com.facebook.presto.operator.aggregation.AggregationFunction;
+import com.facebook.presto.operator.aggregation.CombineFunction;
+import com.facebook.presto.operator.aggregation.InputFunction;
+import com.facebook.presto.operator.aggregation.OutputFunction;
+import com.facebook.presto.operator.aggregation.state.LongAndDoubleState;
+import com.facebook.presto.operator.aggregation.state.LongState;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.type.BigintType;
+import com.facebook.presto.spi.type.StandardTypes;
+import com.facebook.presto.type.SqlType;
+
+@AggregationFunction("countIf2")
+public class CountIfTwoAggregateFunction
+{
+    @InputFunction
+    public static void input(LongState state, @SqlType(StandardTypes.BIGINT) long value)
+    {
+        if (value == 2L) {
+            state.setLong(state.getLong() + 1);
+        }
+    }
+
+    @CombineFunction
+    public static void combine(LongState state, LongAndDoubleState otherState)
+    {
+        state.setLong(state.getLong() + otherState.getLong());
+    }
+
+    @OutputFunction(StandardTypes.BIGINT)
+    public static void output(LongState state, BlockBuilder out)
+    {
+        BigintType.BIGINT.writeLong(out, state.getLong());
+    }
+}
