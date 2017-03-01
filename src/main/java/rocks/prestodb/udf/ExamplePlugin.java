a4b135e267475f8db426e085e6f9a5d91b8b779a
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-03-01 14:31:20 +0100

    Rename packages

--- src/main/java/rocks/prestodb/udf/ExamplePlugin.java
+++ src/main/java/rocks/prestodb/udf/ExamplePlugin.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.teradata.functions;
+package rocks.prestodb.udf;
 
 import com.facebook.presto.metadata.FunctionFactory;
 import com.facebook.presto.spi.Plugin;
